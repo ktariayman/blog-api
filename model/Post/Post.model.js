6b98594aeb0ec57034453d31c3f859c7262a213c
Ktari ayman <aymanktari31@gmail.com>
2023-01-22 14:24:16 +0100

    models

---
+++ model/Post/Post.model.js
@@ -0,0 +1,63 @@
+const mongoose = require("mongoose");
+
+//create schema
+const postSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Post Title is required"],
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: [true, "Post description is required"],
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: [true, "Post category is required"],
+    },
+    numViews: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    disLikes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+
+    comments: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "Please Author is required"],
+    },
+    photo: {
+      type: String,
+      required: [true, "Post Image is required"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+//Compile the post model
+const Post = mongoose.model("Post", postSchema);
+
+module.exports = Post;
---
+++ model/User/User.model.js
@@ -0,0 +1,102 @@
+const mongoose = require("mongoose");
+const Post = require("../Post/Post.model.js");
+
+//create schema
+const userSchema = new mongoose.Schema(
+  {
+    firstname: {
+      type: String,
+      required: [true, "First Name is required"],
+    },
+    lastname: {
+      type: String,
+      required: [true, "Last Name is required"],
+    },
+    profilePhoto: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+
+    isBlocked: {
+      type: Boolean,
+      default: false,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    role: {
+      type: String,
+      enum: ["Admin", "Guest", "Editor"],
+    },
+    viewers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    followers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    following: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    posts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post",
+      },
+    ],
+    comments: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
+    blocked: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    plan: {
+      type: String,
+      enum: ["Free", "Premium", "Pro"],
+      default: "Free",
+    },
+
+    userAward: {
+      type: String,
+      enum: [
+        "Iron",
+        "Bronze",
+        "Silver",
+        "Gold",
+        "Platinum ",
+        "Diamond",
+        "❤️Legendary",
+      ],
+      default: " Iron",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
