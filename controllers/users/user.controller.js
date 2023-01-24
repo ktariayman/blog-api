7ca5c027cf14f6e99d851ec1a19856b2449ff16c
Ktari ayman <aymanktari31@gmail.com>
2023-01-24 11:07:21 +0100

    uploadPhotoProfile controller

--- controllers/users/user.controller.js
+++ controllers/users/user.controller.js
@@ -83,12 +83,39 @@ const updateUser = async (req, res) => {
 const uploadPhotoProfile = async (req, res) => {
   console.log(req.file);
   try {
+    // find user to be uploaded
+    const userToUpdate = await User.findById(req.userAuth);
+    // check if user if found
+    if (!userToUpdate) {
+      return next(errorHandler("user not found", 403));
+    }
+    // check if user if blocked
+    if (userToUpdate.isBlocked) {
+      return next(
+        errorHandler("Action not allowed, your account is blocked", 403)
+      );
+    }
+    //check if a user is updating their photo
+    if (req.file) {
+      await User.findByIdAndUpdate(
+        req.userAuth,
+        {
+          $set: {
+            profilePhoto: req.file.path,
+          },
+        },
+        {
+          new: true,
+        }
+      );
+    }
+    // updare profile photo
     res.json({
       status: "success",
-      data: "profil photo Upload",
+      data: "profil photo updated successfully",
     });
   } catch (error) {
-    next(errorHandler(error.message));
+    next(errorHandler(error.message, 500));
   }
 };
 const deleteUser = async (req, res) => {
