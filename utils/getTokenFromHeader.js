77ae9b45b59b809ad8769ac23eed347d0776e3fd
Ktari ayman <aymanktari31@gmail.com>
2023-01-28 10:25:04 +0100

    the existence of authorization :getTokenFromHeader

--- utils/getTokenFromHeader.js
+++ utils/getTokenFromHeader.js
@@ -1,5 +1,9 @@
-const getTokenFromHeader = (req) => {
+const { errorHandler } = require("./errorHandler");
+
+const getTokenFromHeader = (req, next) => {
   const header = req.headers;
+  if (!header.authorization)
+    return next(errorHandler("no token in the header", 500));
   const token = header["authorization"].split(" ")[1];
   if (!token) return false;
   return token;
