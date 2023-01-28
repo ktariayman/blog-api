1f19165148780c39ae3de12e69dbdea89f710334
Ktari ayman <aymanktari31@gmail.com>
2023-01-28 13:02:55 +0100

    fix isLogin

--- middlewares/isLogin.js
+++ middlewares/isLogin.js
@@ -2,7 +2,7 @@ const { errorHandler } = require("../utils/ErrorHandler");
 const getTokenFromHeader = require("../utils/getTokenFromHeader");
 const verifyToken = require("../utils/verifyToken");
 const isLogin = (req, res, next) => {
-  const token = getTokenFromHeader(req);
+  const token = getTokenFromHeader(req, next);
   const decodedUser = verifyToken(token);
   req.userAuth = decodedUser.id;
   if (!decodedUser) {
