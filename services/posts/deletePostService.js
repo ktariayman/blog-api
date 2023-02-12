2bd62d6fe6b8b80be26d8ab8eca575d0fa2425df
Ktari ayman <aymanktari31@gmail.com>
2023-02-12 15:37:46 +0100

    implement deletePostService

--- services/posts/deletePostService.js
+++ services/posts/deletePostService.js
@@ -2,8 +2,18 @@ const Post = require("../../model/Post/Post.model");
 const User = require("../../model/User/User.model");
 const { errorHandler } = require("../../utils/ErrorHandler");
 
-const deletePost = async (req, res) => {
+const deletePost = async (req, res, next) => {
   try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return next(
+        errorHandler("post with id: " + req.params.id + "does not exist ", 403)
+      );
+    }
+    if (post.user.toString() !== req.userAuth.toString()) {
+      return next(errorHandler("not allowed to delete ", 403));
+    }
+    await Post.findByIdAndDelete(req.params.id);
     res.json({
       status: "success",
       data: "delete post",
