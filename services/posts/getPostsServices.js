5aa73036a2f8070d5adb3c1cdf3449d7516d36d7
Ktari ayman <aymanktari31@gmail.com>
2023-02-12 15:38:20 +0100

    complete  getPostsService

--- services/posts/getPostsServices.js
+++ services/posts/getPostsServices.js
@@ -5,8 +5,20 @@ const { errorHandler } = require("../../utils/ErrorHandler");
 const getPosts = async (req, res) => {
   try {
     const posts = await Post.find({})
-      .populate("users")
+      .populate("user")
       .populate("category", "title");
+
+    //  check if the user is blocked by the post owner
+    const filteredPosts = posts.filter((post) => {
+      const blockedUsers = post.user?.blocked;
+      console.log("blockedUsers", blockedUsers);
+      if (blockedUsers === undefined) {
+        const isBlocked = null;
+        return isBlocked;
+      }
+      const isBlocked = blockedUsers.includes(req.userAuth);
+      if (!isBlocked) return post;
+    });
     res.json({
       status: "success",
       data: posts,
