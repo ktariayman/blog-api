b28d395afb1f7e6eaf35c26ca96cd657579a5d25
Ktari ayman <aymanktari31@gmail.com>
2023-02-12 15:36:41 +0100

    fix posts routes + export / import controllers

--- controllers/posts/postsController.js
+++ controllers/posts/postsController.js
@@ -3,6 +3,8 @@ const getPosts = require("../../services/posts/getPostsServices");
 const getPost = require("../../services/posts/getPostService");
 const updatePost = require("../../services/posts/updatePostService");
 const deletePost = require("../../services/posts/deletePostService");
+const likePost = require("../../services/posts/likePostsService");
+const dislikePost = require("../../services/posts/dislikePostService");
 
 exports.controllers = {
   createPost,
@@ -10,4 +12,6 @@ exports.controllers = {
   getPost,
   updatePost,
   deletePost,
+  likePost,
+  dislikePost,
 };
--- routes/posts/postRoutes.js
+++ routes/posts/postRoutes.js
@@ -1,21 +1,38 @@
 const express = require("express");
 const postRouter = express.Router();
-
+const isLogin = require("../../middlewares/isLogin");
+const storage = require("../../config/cloudinary");
+const multer = require("multer");
 const posts = require("../../controllers/posts/postsController");
 
-const isLogin = require("../../middlewares/isLogin");
-
+const upload = multer({ storage });
 //POST/api/v1/posts
-postRouter.post("/", isLogin, posts.controllers.createPost);
+postRouter.post(
+  "/",
+  isLogin,
+  upload.single("image"),
+  posts.controllers.createPost
+);
 
 //GET/api/v1/posts
 postRouter.get("/", isLogin, posts.controllers.getPosts);
+
+//GET/api/v1/posts/likes/:id
+postRouter.get("/likes/:id", isLogin, posts.controllers.likePost);
+
+//GET/api/v1/posts/dislikes/:id
+postRouter.get("/dislikes/:id", isLogin, posts.controllers.dislikePost);
 
 //GET/api/v1/posts/:id
 postRouter.get("/:id", isLogin, posts.controllers.getPost);
 
 //PUT/api/v1/posts/:id
-postRouter.put("/:id", isLogin, posts.controllers.updatePost);
+postRouter.put(
+  "/:id",
+  isLogin,
+  upload.single("image"),
+  posts.controllers.updatePost
+);
 
 //DELETE/api/v1/posts/:id
 postRouter.delete("/:id", isLogin, posts.controllers.deletePost);
