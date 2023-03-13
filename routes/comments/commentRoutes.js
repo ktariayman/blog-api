0566e0062806630736dde7910cc2a0567325dfdd
Aymanktari <aymanktari31@gmail.com>
2023-03-13 22:02:36 +0100

    commentRoutes

--- routes/comments/commentRoutes.js
+++ routes/comments/commentRoutes.js
@@ -1,18 +1,17 @@
-const express = require("express");
-const comments = require("../../controllers/comments/commentsController");
-
+const express = require('express');
+const comments = require('../../controllers/comments/commentsController');
 const commentRouter = express.Router();
-
+const isLogin = require('../../middlewares/isLogin');
 //POST/api/v1/comments
-commentRouter.post("/", comments.controllers.createComment);
+commentRouter.post('/:id', isLogin, comments.controllers.createComment);
 
 //GET/api/v1/comments/:id
-commentRouter.get("/:id", comments.controllers.getComment);
+commentRouter.get('/:id', comments.controllers.getComment);
 
 //PUT/api/v1/comments/:id
-commentRouter.put("/:id", comments.controllers.updateComment);
+commentRouter.put('/:id', isLogin, comments.controllers.updateComment);
 
 //DELETE/api/v1/comments/:id
-commentRouter.delete("/:id", comments.controllers.deleteComment);
+commentRouter.delete('/:id', isLogin, comments.controllers.deleteComment);
 
 module.exports = commentRouter;
